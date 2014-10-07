351fe44f9d638b6c30292faf0a31c6a9602d1f71
Totto <totto@totto.org>
2014-10-07 18:02:14 +0200

    added patient names

--- src/main/java/com/altran/iot/gui/InsideController.java
+++ src/main/java/com/altran/iot/gui/InsideController.java
@@ -30,6 +30,11 @@ public class InsideController {
     private String patient3 = "001BC50C71000017";
     private String patient4 = "001BC50C7100001F";
     private String patient5 = "001BC50C7100001A";
+    private String namepatient1 = "Lise";
+    private String namepatient2 = "Petter";
+    private String namepatient3 = "Ole";
+    private String namepatient4 = "Knut";
+    private String namepatient5 = "Reidun";
 
     Random randomGenerator = new Random();
 
@@ -40,17 +45,17 @@ public class InsideController {
 
         Map model = new HashMap<String, String>();
         model.put(HEADING, "Pasientovervåkning - demente");
-        model.put(PATIENT_1, "Inside - (A: 50, B: 30, C:45)  - " + patient1);
-        model.put(PATIENT_2, "Inside - (A: 50, B: 40, C:45)  - " + patient2);
+        model.put(PATIENT_1, namepatient1 + " Inside - (A: 50, B: 30, C:45)  - " + patient1);
+        model.put(PATIENT_2, namepatient2 + " Inside - (A: 50, B: 40, C:45)  - " + patient2);
 
         if (randomGenerator.nextInt(100) > 50) {
-            model.put(PATIENT_3, "Inside - (A: 50, B: 36, C:65)  - " + patient3);
-            model.put(PATIENT_4, "<font color=\"red\">Outside - (A: 50, B: 30, C:13)  - " + patient4 + "</font>");
+            model.put(PATIENT_3, namepatient3 + " Inside - (A: 50, B: 36, C:65)  - " + patient3);
+            model.put(PATIENT_4, namepatient4 + " <font color=\"red\">Outside - (A: 50, B: 30, C:13)  - " + patient4 + "</font>");
         } else {
-            model.put(PATIENT_3, "<font color=\"red\">Outside - (A: 50, B: 40, C:?)  - " + patient3 + "</font>");
-            model.put(PATIENT_4, "Inside - (A: 50, B: 36, C:65)  - " + patient4);
+            model.put(PATIENT_3, namepatient3 + " <font color=\"red\">Outside - (A: 50, B: 40, C:?)  - " + patient3 + "</font>");
+            model.put(PATIENT_4, namepatient4 + " Inside - (A: 50, B: 36, C:65)  - " + patient4);
         }
-        model.put(PATIENT_5, "<font color=\"red\">Outside - (A: ?, B: ?, C:?)  - " + patient5 + "</font> ");
+        model.put(PATIENT_5, namepatient5 + " <font color=\"red\">Outside - (A: ?, B: ?, C:?)  - " + patient5 + "</font> ");
 
         model.put(CORNER_1, "A: 50, B: 30, C:45");
         model.put(CORNER_2, "A: 50, B: 80, C:45");
