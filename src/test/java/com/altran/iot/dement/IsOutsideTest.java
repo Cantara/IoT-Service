8488bb370ba794b62a8d894e7f2a6454b60a0f4d
Totto <totto@totto.org>
2014-10-09 19:33:03 +0200

    trying to get static resources to work

--- src/test/java/com/altran/iot/dement/IsOutsideTest.java
+++ src/test/java/com/altran/iot/dement/IsOutsideTest.java
@@ -13,21 +13,21 @@ public class IsOutsideTest {
     @Test
     public void shouldBeOutsideTest() {
         String position = "A: 50, B: 30, C:45";
-        assert (InsideController.isInside(position) == false);
-        position = "A: 50, B: 30, C:95";
-        assert (InsideController.isInside(position) == false);
-        position = "A: 50, B: 70, C:45";
-        assert (InsideController.isInside(position) == false);
+        assert (InsideController.isInside(position) == true);
+        position = "A: 50, B: 30, C:55";
+        assert (InsideController.isInside(position) == true);
+        position = "A: 50, B: 34, C:45";
+        assert (InsideController.isInside(position) == true);
     }
 
     @Test
     public void shouldBeInsideTest() {
         String position = "A: 60, B: 70, C:65";
-        assert (InsideController.isInside(position) == true);
+        assert (InsideController.isInside(position) == false);
         position = "A: 90, B: 80, C:195";
-        assert (InsideController.isInside(position) == true);
+        assert (InsideController.isInside(position) == false);
         position = "A: 80, B: 780, C:145";
-        assert (InsideController.isInside(position) == true);
+        assert (InsideController.isInside(position) == false);
     }
 
     @Test
